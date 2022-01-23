7a97bdb872d5da1c9245577436754f02da74f764
Shivam Bhilarkar <shivambhilarkar@gmail.com>
2022-01-23 14:42:39 +0530

    ...

---
+++ Linear Searching/src/Q10_find_pair_with_Min_Absoulute_Sum.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+//given an array of integers of both +ve & -ve values.
+//find two elements in the array such that their sum is minimum.
+public class Q10_find_pair_with_Min_Absoulute_Sum {
+      public static void find_pair1(int[] arr){
+
+            Arrays.sort(arr);
+            int start = 0;
+            int end = arr.length-1;
+            int a = -1, b = -1;
+            int minsum = Integer.MAX_VALUE;
+            while(start <  end){
+                  int sum = Math.abs(arr[start] + arr[end]);
+                  if(sum < minsum){
+                        minsum = sum;
+                        a = arr[start];
+                        b = arr[end];
+                  }
+                  if(sum < 0){
+                        start++;
+                  }else{
+                        end--;
+                  }
+            }
+            System.out.println("a : "+a + "\nb : "+b + "\nsum :"+minsum);
+      }
+      public static void main(String[] args){
+            Scanner scan = new Scanner (System.in);
+            int[] arr = {1,-3,-10,6,18, 9,-15};
+            find_pair1 (arr);
+      }
+}
---
+++ Linear Searching/src/Q2_find_missing_in_range_minmax.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+//find min max from array & find missing values from min-max;
+public class Q2_find_missing_in_range_minmax {
+
+      public static void find_missing(int[] arr){
+            int min = Integer.MAX_VALUE;
+            int max = Integer.MIN_VALUE;
+
+            Arrays.sort(arr);
+            ArrayList<Integer> list = new ArrayList<> ();
+            int value = arr[0];
+            int i = 0;
+            while(i < arr.length){
+                  if(value == arr[i]){
+                        value += 1;
+                        i += 1;
+                  }else{
+                        list.add(value);
+                        value += 1;
+                  }
+            }
+            System.out.println("Missing Numbers : "+list);
+      }
+
+      public static void main(String[] args){
+            Scanner scan = new Scanner (System.in);
+
+            int[] arr = {1,2,3,4,5,6,8,9};
+            find_missing (arr);
+
+      }
+
+}
